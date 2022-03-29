cd87a3dd8e7a6f9b0879c95623d95af099d88426
Mohammad Ismaeel <50014764+ab3adel@users.noreply.github.com>
2022-03-29 18:53:38 +0300

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React ,{Suspense}from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import '@fontsource/roboto/300.css';
@@ -9,12 +9,17 @@ import App from './App';
 import {BrowserRouter as Router} from 'react-router-dom'
 import {Provider} from 'react-redux'
 import {store} from './store/store'
+import {CircularProgress} from '@mui/material'
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
 
       <Router>
-        <App />
+        <Suspense fallback={()=><CircularProgress  sx={{position:'absolute',top:'50%',left:'50%'}}
+                                  size={40} />}>
+
+          <App />
+        </Suspense>
       </Router>
     </Provider>
   </React.StrictMode>,
