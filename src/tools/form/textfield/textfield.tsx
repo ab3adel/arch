e58a52f025e1cc63f525c7bf628531e38129a7af
Mohammad Ismaeel <50014764+ab3adel@users.noreply.github.com>
2022-03-29 15:01:32 +0300

    Add files via upload

---
+++ src/tools/form/textfield/textfield.tsx
@@ -0,0 +1,26 @@
+import './textfield.scss'
+import {TextareaAutosize} from '@mui/material'
+import React, {useState} from 'react'
+interface iProps {label:string,name:string,formFields:any,setFormField:Function}
+export const TextArea =({label,name,formFields,setFormField}:iProps) =>{
+const [val,setVal] =useState('')
+const handleChange = (e:React.ChangeEvent) =>{
+
+    let input = e.target as HTMLInputElement
+    console.log (input.value,'Message')
+    setFormField('Message',input.value,false)
+}
+    return (
+        <textarea
+        onChange={(e:React.ChangeEvent)=>handleChange(e)}
+        value={formFields[name]}
+        className='textArea'
+        aria-label={label}
+        placeholder={label}
+        style={{border:formFields[`${name}Error`]?'1px solid red':''}}
+        >
+
+        </textarea>
+         
+    )
+}
