32ce5b4f319f70f825636b09a3ea37810f8b2032
Mohammad Ismaeel <50014764+ab3adel@users.noreply.github.com>
2022-03-29 15:55:16 +0300

    Update footer.tsx

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -5,8 +5,11 @@ import {dummy,isEmail} from '../../tools/validations/validations'
 import {motion ,Variants} from 'framer-motion'
 import {ArrowRightAlt,Copyright} from '@mui/icons-material'
 import {useInView} from 'react-intersection-observer'
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import {useAnimation} from 'framer-motion'
+import {Apis,Url} from '../../tools/apis/apis'
+import { AlertColor, CircularProgress} from '@mui/material'
+import {Notification} from '../../tools/notification/notification'
 const btn:Variants = {
     first : {
         x:200
@@ -26,14 +29,90 @@ const btn:Variants = {
         }
     }
 }
+interface iFields {[key:string]:string|boolean}
 export const Footer =()=>{
 const [ref,inView] =useInView()
 const control= useAnimation()
+const [fields,setFields]=useState<iFields>({
+    Name:'',NameError:false,
+    Company:'',CompanyError:false,
+    Email:'',EmailError:false,
+    Subject:'',SubjectError:false,
+    Message:'',MessageError:false
+})
+const [loading,setLoading]= useState(false)
+const [notification,setNotification]=useState({
+    show:false,
+    severity:'success',
+    message:''
+})
+const handleField =(name:string,value:string,err:boolean)=>{
+    let newFields= {...fields}
+    newFields[name]=value
+    setFields(pre=>({...pre,...newFields}))
+}
 useEffect(()=>{
 if (inView) {
     control.start('second')
 }
 },[inView])
+const emptyFields= ()=>{
+let newFields= {...fields}
+Object.keys(newFields).map(ele=>{
+    newFields[ele]=''
+})
+setFields(pre=>({...pre,...newFields}))
+}
+
+const checkErro =()=>{
+    let newFields= {...fields}
+    let errors=Object.keys(newFields).filter (ele=>
+        !ele.includes('Error') && ele !== "Subject" && !newFields[ele] 
+    )
+    errors.map(ele=>{
+       newFields[`${ele}Error`]=true
+    })
+    setFields(pre=>({...pre,...newFields}))
+    console.log(errors)
+    if (errors.length>0) return true
+    return false 
+}
+
+const sendMessage =async() =>{
+    if (checkErro()) return 
+let formData= new FormData()
+formData.append('name',fields['Name'] as string)
+formData.append('email',fields['Email'] as string)
+formData.append('company',fields['Company'] as string)
+formData.append('subject',"message")
+formData.append('message',fields['Message'] as string)
+setLoading(true)
+await fetch(Url+Apis.message,{
+    method:'POST',
+    body:formData
+})
+.then(res=>{
+    if (res.ok) {
+        setNotification(pre=>({...pre,show:true
+                       ,message:'your message sent successfully',severity:'success'}))
+        setLoading(false)
+        emptyFields()  
+
+    }
+    else {
+        setNotification(pre=>({...pre,show:true
+            ,message:'your message wasn not send',severity:'error'}))
+        setLoading(false)
+    }
+})
+.catch(err=>{
+    setLoading(false)
+    console.log(err)
+})
+}
+const handleClose=() =>{
+    setNotification(pre=>({...pre,show:false}))
+}
     return (
         <div className="footerContainer" ref={ref}>
             <div className="footerTitle">
@@ -56,16 +135,21 @@ if (inView) {
                 </div>
                 <div className="col2">
                     <div className="inputGroups">
-                        <Input type='text' label='Name'
+                        <Input type='text' label='Name' formFields={fields}
+                        setFormField={handleField}
                         validator={dummy} name='Name' />
                         <Input type='number' label='Phone'
+                         formFields={fields} setFormField={handleField}
                         validator={dummy} name='Phone' />
                         <Input type='text' label='Company'
+                        formFields={fields} setFormField={handleField}
                         validator={dummy} name='Company' />
                         <Input type='email' label='Email'
+                        formFields={fields} setFormField={handleField}
                         validator={isEmail} name='Email' />
                     </div>
-                    <TextArea  label='message'  />
+                    <TextArea  label='message'  formFields={fields} 
+                    setFormField={handleField} name={'Message'}/>
                 </div>
                 <div className="footerButton">
                     <div className="sendButton">
@@ -76,9 +160,14 @@ if (inView) {
                          whileInView={'second'}
                          viewport={{once:true}}
                          animate={control}
+                         onClick={()=>sendMessage()}
                          >
-                            SEND 
-                            <ArrowRightAlt color="inherit" fontSize='inherit'/>
+                             {loading ? <CircularProgress  />:
+                            <>
+                            SEND <ArrowRightAlt color="inherit" fontSize='inherit'/>
+                            </> }
+                           
+                          
                         </motion.button>
                     </div>
                 </div>
@@ -89,6 +178,8 @@ if (inView) {
                 <p>by Ikoniks Software</p>
                 <p><Copyright/> all Rights Reserved</p>
             </div>
+            {notification.show && (<Notification show={notification.show} handleClose={handleClose}
+                                    severity={notification.severity as AlertColor} message={notification.message}/>)}
         </div>
     )
 }
