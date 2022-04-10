6d09890716d8503cdb750c63a7232c7284fff15c
Mohammad Ismaeel <50014764+ab3adel@users.noreply.github.com>
2022-04-10 16:32:19 +0300

    delete section 2

--- src/components/homepage/homepage.tsx
+++ src/components/homepage/homepage.tsx
@@ -1,6 +1,6 @@
 import './homepage.scss'
 import {HomePageSectionOne} from './homepage-section-one/homepage-section-one'
-import {HomepageSectionTwo} from './homepage-section-two/homepage-section-two'
+
 import {HomepageSectionThree} from './homepage-section-three/homepage-section-three'
 import {HomepageSectionFour} from './homepage-section-four/homepage-section-four'
 import {HomepageSectionFive} from './homepage-section-five/homepage-section-five'
@@ -19,7 +19,7 @@ export const HomePage =() =>{
           
             <HomePageSectionOne 
                 />
-            {/* <HomepageSectionTwo/> */}
+       
             <HomepageSectionThree />
             <HomepageSectionFour/>
             <HomepageSectionFive/>
